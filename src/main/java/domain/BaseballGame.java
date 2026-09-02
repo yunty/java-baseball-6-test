92615f1051280f7a77840e6910da81b752e0fee5
yunty <yty9024@naver.com>
2026-09-02 22:24:58 +0900

    refactor : 반복이 되지 않는 오류 해결

--- src/main/java/domain/BaseballGame.java
+++ src/main/java/domain/BaseballGame.java
@@ -3,23 +3,36 @@ package domain;
 import java.util.List;
 
 public class BaseballGame {
-    private final List<Integer> randomNumbers;
+    private List<Integer> randomNumbers;
+    private Referee referee;
 
     public BaseballGame() {
         this.randomNumbers = RandomGenerator.getNumber();
+        referee = new Referee();
     }
 
-    public List<Integer> intToList(int numbers) {
-        return String.valueOf(numbers)
-                .chars()
+    public List<Integer> intToList(String input) {
+        return input.chars()
                 .map(c -> c - '0')
                 .boxed()
                 .toList();
     }
 
-    public void play(int numbers, Referee referee) {
-        List<Integer> userNumbers = intToList(numbers);
+    public void play(String input) {
+        List<Integer> userNumbers = intToList(input);
         referee.judge(randomNumbers, userNumbers);
+    }
+
+    public int getStrike() {
+        return referee.getStrike();
+    }
+
+    public int getBall() {
+        return referee.getBall();
+    }
+
+    public boolean isStrikeOut() {
+        return referee.isStrikeOut();
     }
 
 
