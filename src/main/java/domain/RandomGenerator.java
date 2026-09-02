c7655d0dad64a6dde684877b500bd60f16981bed
yunty <yty9024@naver.com>
2026-09-02 15:25:45 +0900

    refactor : 요구사항에 맞춰서 Randoms API사용하여 랜덤 숫자 추출 기능 구현/RandomGenerator.java

--- src/main/java/domain/RandomGenerator.java
+++ src/main/java/domain/RandomGenerator.java
@@ -1,19 +1,18 @@
 package domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class RandomGenerator {
 
     public static List<Integer> getNumber() {
         List<Integer> randomNumbers = new ArrayList<>();
-        Random random = new Random();
         int index = 0;
         int randomNumber;
 
         while (index < 3) {
-            randomNumber = random.nextInt(9) + 1;
+            randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!randomNumbers.contains(randomNumber)) {
                 randomNumbers.add(randomNumber);
                 index++;
