4ad3e3f877a305910346d7459a16f39e1eb2b722
yunty <yty9024@naver.com>
2026-09-02 22:29:39 +0900

    feat : 게임 실행 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import View.InputView;
+import View.OutputView;
+import controller.BaseballController;
+import domain.BaseballGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        BaseballGame baseballGame = new BaseballGame();
+        BaseballController baseball = new BaseballController(inputView, outputView, baseballGame);
+
+        baseball.gameStart();
     }
 }
