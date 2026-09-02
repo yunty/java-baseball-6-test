ee60bbba6a280f5352122c7bb3c2ee6b19a30555
yunty <yty9024@naver.com>
2026-09-02 22:27:19 +0900

    refactor : 낫싱 출력 구현

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -1,7 +1,7 @@
 package View;
 
 public class OutputView {
-    
+
     public void printStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
@@ -11,6 +11,7 @@ public class OutputView {
     }
 
     public void printExitOrAgain() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
@@ -21,6 +22,9 @@ public class OutputView {
         }
         if (strike != 0) {
             result.append(strike + "스트라이크");
+        }
+        if (ball == 0 && strike == 0) {
+            result.append("낫싱");
         }
         System.out.println(result.toString());
     }
